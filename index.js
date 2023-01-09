22d4e9a7be5fafe5de49c330c3b6055a2b1b54ba
Gowtham A <117510786+Gowtham2141999@users.noreply.github.com>
2023-01-09 23:48:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const navBar = document.querySelector('ul');
+
+hamburger.addEventListener('click', ()=>{
+navBar.classList.toggle('slide')
+});
